b6be7c7ac7a2ddb9521baf2da4966d3bfc715e41
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-29 14:17:01 -0500

    methods

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+      title: "hello world",
+    }
   }
+  {
+  this.setstate({ isLoaded: true });}
 
   render() {
     return (
       <div>
         <h1>Welcome to React!</h1>
+        <input id ="title"
+        name ="title"
+        />
         <hr />
       </div>
     );
+    else{
+      <h1>loading .....</h1>
+    }
   }
+
 }
 
 export default App;
